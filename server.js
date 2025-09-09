dbc6f68fba66d3ac81923a82d98428021481019c
Natan <natandesouza403@gmail.com>
2025-09-09 19:21:35 -0300

    ajustes .env

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const path = require("path");
+require("dotenv").config();
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 app.use("/", require("./routes/routes"));
 
-app.listen(9001);
-console.log("http://localhost:9001/api/relacao/");
+app.listen(port, () => {
+  console.log(`Rodando na porta ${port}`);
+});
