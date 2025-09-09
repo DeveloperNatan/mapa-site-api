bc1fcfd73b84c1128f07c2bf5d32854cd43a16d4
DeveloperNatan <natandesouza403@gmail.com>
2025-09-09 13:10:48 -0300

    1.0

--- routes/routes.js
+++ routes/routes.js
@@ -10,9 +10,4 @@ router.get("/api/relacao/", async function (req, res) {
   await service.EncontrarTodos(req, res);
 });
 
-router.put("/api/relacao/:id", async function (req, res) {
-  await service.sincronizarHistorico(req, res);
-  await service.AtualizarRelacionamento(req, res);
-});
-
 module.exports = router;
--- service/service.js
+++ service/service.js
@@ -30,64 +30,19 @@ exports.EncontrarTodos = async function (req, res) {
   try {
     const localizacoesComrelacao = await prisma.localizacaoPA.findMany({
       include: {
-        RelacionamentoPA: true,
+        RelacionamentoPA: {
+          include: {
+            HistoricoPA: true,
+          }
+        },
       },
     });
     res.json(localizacoesComrelacao);
   } catch (error) {
     res
       .status(404)
-      .json({ error: "erro ao criar produto", details: error.message });
+      .json({ error: "erro ao encontrar", details: error.message });
   }
 };
 
-exports.AtualizarRelacionamento = async function (req, res) {
-  try {
-    const id = parseInt(req.params.id); // ID do RelacionamentoPA
-    const dadosNovos = req.body; // campos enviados pelo cliente
-    console.log("dadosNovos:", dadosNovos);
-    console.log("atual:", atual);
 
-    // 1️⃣ Buscar o estado atual
-    const atual = await prisma.relacionamentoPA.findUnique({
-      where: { id },
-      include: { localizacaoPA: true },
-    });
-
-    if (!atual) {
-      return res.status(404).json({ error: "Relacionamento não encontrado" });
-    }
-
-    // 2️⃣ Comparar campos e criar histórico se houver alteração
-    const camposParaMonitorar = [
-      "patrimonioPC",
-      "patrimonioMNT1",
-      "patrimonioMNT2",
-    ];
-    for (const campo of camposParaMonitorar) {
-      if (dadosNovos[campo] && dadosNovos[campo] !== atual[campo]) {
-        await prisma.historicoPA.create({
-          data: {
-            relacionamentoPAId: atual.id,
-            localizacaoPAId: atual.localizacaoPA.id,
-            acao: "ALTERACAO",
-            valorAnterior: atual[campo],
-            valorNovo: dadosNovos[campo],
-          },
-        });
-      }
-    }
-
-    // 3️⃣ Atualizar o relacionamentoPA
-    const atualizado = await prisma.relacionamentoPA.update({
-      where: { id },
-      data: dadosNovos,
-    });
-
-    res.json(atualizado);
-  } catch (error) {
-    res
-      .status(400)
-      .json({ error: "Erro ao atualizar", details: error.message });
-  }
-};
